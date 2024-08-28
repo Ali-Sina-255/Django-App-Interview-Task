c55d8888bddc25adaa47394012029c554de87a68
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-08-28 18:28:10 +0430

    requirement is updated;;;

--- accounts/models.py
+++ accounts/models.py
@@ -13,7 +13,7 @@ class UserManager(BaseUserManager):
             last_name=last_name,
         )
         user.set_password(password)
-        user.is_active = False  # Make user inactive until email is verified
+        user.is_active = False 
         user.save(using=self._db)
         return user
 
@@ -25,10 +25,10 @@ class UserManager(BaseUserManager):
             last_name=last_name,
         )
         user.is_admin = True
-        user.is_active = True  # Superuser is always active
+        user.is_active = True  
         user.is_staff = True
         user.is_superadmin = True
-        user.is_email_verified = True  # Superuser email is considered verified
+        user.is_email_verified = True 
         user.save(using=self._db)
         return user
 
--- api/views.py
+++ api/views.py
@@ -1,33 +1,26 @@
-
-from django.contrib.auth  import get_user_model
+from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect, get_object_or_404
-
 from django.contrib.auth.decorators import login_required
-from django.utils import timezone
-from django_filters.rest_framework import DjangoFilterBackend
 
-from rest_framework import generics, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics, permissions, viewsets, status
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.exceptions import PermissionDenied, NotFound
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework import viewsets
-
-
-from .models import Job
-from . forms import CommandForm
-from .filters import JobFilter
-from . paginations import DefaulPagination
-from .serializers import JobSerializer, JobResultSerializer, RegisterSerializer, VerifyEmailSerializer,LoginSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics,status
-from accounts.tasks import send_verification_email_task , execute_command_task
+
+from .models import Job, JobResult
+from .forms import CommandForm
+from .filters import JobFilter
+from .paginations import DefaulPagination
+from .serializers import JobSerializer, JobResultSerializer, RegisterSerializer, VerifyEmailSerializer, LoginSerializer
+from accounts.tasks import send_verification_email_task, execute_command_task
 
 
 class JobViewSet(viewsets.ModelViewSet):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
-    
 
 
 class JobListCreateView(generics.ListCreateAPIView):
@@ -35,12 +28,12 @@ class JobListCreateView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     authentication_classes = [JWTAuthentication]
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    filterset_fields = ['status'] 
+    filterset_fields = ['status']
     filterset_class = JobFilter
     pagination_class = DefaulPagination
-    search_fields = ['name','description','stats']
-    ordering_fields = ['name','price','scheduled_time','updated_at']
-    
+    search_fields = ['name', 'description', 'stats']
+    ordering_fields = ['name', 'price', 'scheduled_time', 'updated_at']
+
     def get_queryset(self):
         if not self.request.user.is_email_verified:
             raise PermissionDenied("User not verified.")
@@ -50,62 +43,54 @@ class JobListCreateView(generics.ListCreateAPIView):
         serializer.save(user=self.request.user)
 
 
-
 class JobDetailView(generics.RetrieveDestroyAPIView):
-	serializer_class = JobSerializer
-	permission_classes = [permissions.IsAuthenticated]
+    serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-	def get_queryset(self):
-		return Job.objects.filter(user=self.request.user)
+    def get_queryset(self):
+        return Job.objects.filter(user=self.request.user)
 
-	def delete(self, request, *args, **kwargs):
-		job = self.get_object()
-		if job.status != 'completed':
-			job.cancel()
-			return Response({"detail": "Job canceled successfully."}, status=204)
-		return Response({"detail": "Cannot cancel a completed job."}, status=400)
-
+    def delete(self, request, *args, **kwargs):
+        job = self.get_object()
+        if job.status != 'completed':
+            job.cancel()
+            return Response({"detail": "Job canceled successfully."}, status=204)
+        return Response({"detail": "Cannot cancel a completed job."}, status=400)
 
 
 class JobUpdateView(generics.UpdateAPIView):
-	serializer_class = JobSerializer
-	permission_classes = [permissions.IsAuthenticated]
-	authentication_classes = [JWTAuthentication]
+    serializer_class = JobSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
 
-	def get_queryset(self):
-		return Job.objects.filter(user=self.request.user)
+    def get_queryset(self):
+        return Job.objects.filter(user=self.request.user)
 
-	def update(self, request, *args, **kwargs):
-		job = self.get_object()
-
-		# Check if the job status is 'completed'
-		if job.status == 'completed':
-			return Response({"detail": "Cannot update a completed job."}, status=400)
-		
-		# If not completed, proceed with the update
-		return super().update(request, *args, **kwargs)
-
+    def update(self, request, *args, **kwargs):
+        job = self.get_object()
+        if job.status == 'completed':
+            return Response({"detail": "Cannot update a completed job."}, status=400)
+        return super().update(request, *args, **kwargs)
 
 
 class JobResultView(generics.RetrieveAPIView):
-	serializer_class = JobResultSerializer
-	permission_classes = [permissions.IsAuthenticated]
+    serializer_class = JobResultSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-	def get_object(self):
-		job = Job.objects.filter(user=self.request.user, pk=self.kwargs['pk']).first()
-		if not job or not job.is_completed():
-			raise NotFound("Job not found or not completed yet.")
-		try:
-			return job.jobresult  
-		except JobResult.DoesNotExist:
-			raise NotFound("Job result not available.")
-
+    def get_object(self):
+        job = Job.objects.filter(user=self.request.user, pk=self.kwargs['pk']).first()
+        if not job or not job.is_completed():
+            raise NotFound("Job not found or not completed yet.")
+        try:
+            return job.jobresult
+        except JobResult.DoesNotExist:
+            raise NotFound("Job result not available.")
 
 
 class RegisterView(generics.CreateAPIView):
-	User = get_user_model()
-	queryset = User.objects.all()
-	serializer_class = RegisterSerializer
+    User = get_user_model()
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
 
 
 class VerifyEmailView(generics.GenericAPIView):
@@ -115,20 +100,17 @@ class VerifyEmailView(generics.GenericAPIView):
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-
         email = serializer.validated_data.get('email')
 
         try:
             user = self.User.objects.get(email=email)
             if not user.is_email_verified:
-                # Send verification email
                 send_verification_email_task.delay(
                     user_id=user.id,
                     email_subject="Verify Your Email Address",
                     email_template="accounts/email/verification_email.html",
-                    domain="localhost://8000"  
+                    domain="localhost://8000"
                 )
-
                 return Response({"detail": "Verification email sent successfully."}, status=status.HTTP_200_OK)
             else:
                 return Response({"detail": "Email is already verified."}, status=status.HTTP_200_OK)
@@ -138,21 +120,20 @@ class VerifyEmailView(generics.GenericAPIView):
 
 
 class LoginView(TokenObtainPairView):
-	serializer_class = LoginSerializer
+    serializer_class = LoginSerializer
 
 
 def all_jobs(request):
-	all_jobs = Job.objects.all()
-	context = {
-		'jobs':all_jobs
-	}
-	return render(request, 'job/job_list.html', context)
+    all_jobs = Job.objects.all()
+    context = {
+        'jobs': all_jobs
+    }
+    return render(request, 'job/job_list.html', context)
 
 
 @login_required
 def job_detail_view(request, pk):
     job = get_object_or_404(Job, pk=pk)
-    
     if request.method == 'POST':
         form = CommandForm(request.POST)
         if form.is_valid():
@@ -160,11 +141,9 @@ def job_detail_view(request, pk):
             command.job = job
             command.owner = request.user.profile
             command.save()
-            
             execute_command_task.delay(command.id)
-            
             return redirect('job-detail-list', pk=pk)
     else:
         form = CommandForm()
-    
+
     return render(request, 'job/job_detail.html', {'job': job, 'form': form})
